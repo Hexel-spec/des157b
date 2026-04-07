8c7dce06b4f53ed0d181e0807433ac9b5caff577
Hexel-spec <hryxu@ucdavis.edu>
2026-04-07 13:02:15 -0700

    pen buttons added

--- script.js
+++ script.js
@@ -11,8 +11,9 @@
         if (mode === 'dark') {
             body.className = 'switch';
             banner.className = 'switch';
-            banner.innerHTML = '<img src="images/home-is-banner-childhood.JPG" alt="childhood home" width="1100" height="250">'
+            banner.innerHTML = '<img src="images/home-is-banner-childhood.JPG" alt="childhood home" width="1100" height="250">';
             button.className = 'switch';
+            button.innerHTML = '<img src="images/switch-button-pen.png" alt="switch button" width="50" height="50">';
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -20,8 +21,9 @@
         } else {
             body.removeAttribute('class');
             banner.removeAttribute('class');
-            banner.innerHTML = '<img src="images/home-is-banner-modern.png" alt="modern home" width="1100" height="250">'
+            banner.innerHTML = '<img src="images/home-is-banner-modern.png" alt="modern home" width="1100" height="250">';
             button.removeAttribute('class');
+            button.innerHTML = '<img src="images/switch-button-crayon.png" alt="switch button" width="50" height="50">';
             for (const section of sections) {
                 section.removeAttribute('class');
             }
